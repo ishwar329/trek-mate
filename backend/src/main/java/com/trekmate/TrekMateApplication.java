8fa891a42e3532532480137bd4c3b6f9d3fced48
ishwar329 <ishwargosavi329@gmail.com>
2026-06-19 17:12:22 +0530

    backend folder add

---
+++ backend/src/main/java/com/trekmate/TrekMateApplication.java
@@ -0,0 +1,10 @@
+package com.trekmate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekMateApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TrekMateApplication.class, args);
+    }
+}
